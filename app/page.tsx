e3f2a1a040e95d6bb51c7522c2b6c5d2007a25a8
keshav-sudo <thesharmakeshav@gmail.com>
2025-10-13 23:21:26 +0530

    portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import AnimatedBackground from '@/components/AnimatedBackground'
-import HeroSection from '@/components/HeroSection'
-import AboutSection from '@/components/AboutSection'
-import GitHubStats from '@/components/GitHubStats'
-import Projects from '@/components/Projects'
-import Footer from '@/components/Footer'
+import AnimatedBackground from '../components/AnimatedBackground'
+import HeroSection from '../components/HeroSection'
+import AboutSection from '../components/AboutSection'
+import GitHubStats from '../components/GitHubStats'
+import Projects from '../components/Projects'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
