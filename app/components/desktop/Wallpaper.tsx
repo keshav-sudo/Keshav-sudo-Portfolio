be9c87ee19ec94f0870173dd8b2ed44033ba4ac9
keshav sharma <keshavsharma@Mac.lan>
2025-12-30 22:27:30 +0530

    new

--- app/components/desktop/Wallpaper.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-export const Wallpaper = () => (
-  <div
-    className="absolute inset-0 bg-cover bg-center"
-    style={{ backgroundImage: "url('/ubuntu-wallpaper.jpg')" }}
-  />
-)
